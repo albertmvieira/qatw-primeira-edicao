c32df1cf66bfb610c1f0e5c37b2b3e08e980b03e
Albert Vieira <albertmvieira@hotmail.com>
2025-02-16 22:04:13 -0300

    Implementação de recuperação do 2FA através de fila do Redis

---
+++ support/redis.js
@@ -0,0 +1,26 @@
+import {Queue} from 'bullmq';
+
+const connection = {
+    host: 'paybank-redis',
+    port: 6379
+}
+
+const queueName = 'twoFactorQueue'
+const queue = new Queue(queueName, { connection })
+let job
+
+// Função que retorna o primeiro job da fila twoFactorQueue
+// outra forma de criar função assíncrona async function getJob()
+export const getJob = async () => {
+    await queue.resume() //resume a fila
+    const jobs = await queue.getJobs() //getJobs retorna um array de jobs
+    console.log(jobs[0].data.code) // imprime o código do primeiro job da fila
+    job = jobs[0].data.code
+    return job // retorna o primeiro job da fila
+}
+
+export const cleanJobs = async () => {
+    //await queue.drain() //drain esvazia a fila
+    //PENDENTE criar validação para apagar a fila somente se não tiver jobs ativos
+    await queue.obliterate() // obliterate apaga a fila
+}
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 import { obterCodigo2FA } from '../support/db';
 import { LoginPage } from '../pages/LoginPage';
 import { DashPage } from '../pages/DashPage';
+import { cleanJobs, getJob } from '../support/redis';
 
 test('Não deve logar quando o código de autenticacão é inválido', async ({ page }) => {
   const loginPage = new LoginPage(page)
@@ -20,7 +21,7 @@ test('Não deve logar quando o código de autenticacão é inválido', async ({ 
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
 });
 
-test('Deve acessar a conta do usuário', async ({ page }) => {
+test('Deve acessar a conta do usuário - pegando o código de confirmação do BD Postgress', async ({ page }) => {
 
   const loginPage = new LoginPage(page)
   const dashPage = new DashPage(page)
@@ -34,15 +35,43 @@ test('Deve acessar a conta do usuário', async ({ page }) => {
   await loginPage.informaCpf(usuario.cpf)
   await loginPage.informaSenha(usuario.senha)
 
-  // temporário
-  await page.waitForTimeout(3000)
-  const codigo = await obterCodigo2FA()
+  await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({timeout: 3000}); //Espera o elemento aparecer na tela até 3 segundos
+  // await page.waitForTimeout(3000) //Não é uma boa prática, mas foi uma solução temporária. Funciona como thread.sleep
+
+  const codigo = await obterCodigo2FA(usuario.cpf)
 
   await loginPage.informe2FA(codigo)
 
-  // temporário
-  await page.waitForTimeout(2000)
+  //inserido await antes do expect para ativar e usar o timeout default de 5000ms do Playwright
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+  
+});
 
-  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+test.only('Deve acessar a conta do usuário - Lendo o código de confirmação da fila do Redis', async ({ page }) => {
+
+  const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
+  
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await cleanJobs()
+  
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+
+  await page.getByRole('heading', { name: 'Verificação em duas etapas' }).waitFor({timeout: 3000}); //Espera o elemento aparecer na tela até 3 segundos
+  // await page.waitForTimeout(3000) //Não é uma boa prática, mas foi uma solução temporária. Funciona como thread.sleep
+
+  //const codigo = await obterCodigo2FA(usuario.cpf)
+  const codigo = await getJob()
+
+  await loginPage.informe2FA(codigo)
+
+  //inserido await antes do expect para ativar e usar o timeout default de 5000ms do Playwright
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
   
 });
